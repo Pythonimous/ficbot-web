5dd7422b1d6a15eafb9b399cb359bf519455417c
Kirill Nikolaev <kir.nikolaev.7@gmail.com>
2022-03-07 23:53:33 +0300

    0.1th iteration of ficbot flask app

--- ficbotweb/__init__.py
+++ ficbotweb/__init__.py
@@ -8,7 +8,6 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
@@ -24,9 +23,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    from . import generate
+    app.register_blueprint(generate.bp)
 
     return app
---
+++ ficbotweb/generate.py
@@ -0,0 +1,52 @@
+import functools
+import logging
+import time
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.utils import secure_filename
+from ficbot.tf_models.name.generation import generate_name
+
+"""
+logging.basicConfig(filename=f"logs/generate/{time.time()}.log",
+                    format='%(asctime)s %(message)s',
+                    level=logging.ERROR)
+"""
+bp = Blueprint('generate', __name__, url_prefix='/generate')
+
+
+@bp.route('/img_name', methods=('GET', 'POST'))
+def img_to_name():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'image' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        image = request.files['image']
+        diversity = float(request.form['diversity'])
+        error = None
+
+        if not image:
+            error = f"{__name__}: Please provide an image."
+
+        filename = 'ficbotweb/static/images/' + str(secure_filename(image.filename))
+        image.save(filename)
+
+        if error is None:
+            # try:
+            name = generate_name(filename,
+                                 "models/img_name/tf/simple.10-1.98.hdf5",
+                                 "models/img_name/tf/maps.pkl",
+                                 diversity=diversity)
+            """
+            except Exception as e:
+                error = f"{__name__}: {str(e)}"
+                logging.error(error)
+            """
+            return render_template("generate/img_name.html",
+                                   char_image=url_for('static', filename=image.filename),
+                                   generated_name=name)
+        flash(error)
+
+    return render_template("generate/img_to_name.html")
